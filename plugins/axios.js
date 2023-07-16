a4f43a7f3ab58a224d25f1966862708e4ed7d53a
Roman <romeoyes777@gmail.com>
2023-07-16 15:21:42 +0300

    feat(): axios added headers

--- plugins/axios.js
+++ plugins/axios.js
@@ -1,5 +1,8 @@
 export default function ({ $axios, i18n }) {
   $axios.onRequest((config) => {
     config.headers.common['Accept-Language'] = i18n.locale
+    config.headers.common["Access-Control-Allow-Origin"] = "*"
+    config.headers.common["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept"
+    config.headers.common["Access-Control-Allow-Methods"] = "GET, POST, PUT"
   })
 }
